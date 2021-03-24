47dc9916441fa87537a0ac08cc090e0dbfc61954
GUNDAANUTEJ <77635770+GUNDAANUTEJ@users.noreply.github.com>
2021-03-24 16:04:01 -0500

    bonus

---
+++ routes/computation.js
@@ -0,0 +1,22 @@
+  
+var express = require('express');
+var router = express.Router();
+var maths;
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    fake_url = "https://fake.com/path" + req.url
+      const url = new URL(fake_url)
+      const search_params = url.searchParams
+
+    
+    maths=Number(search_params.get("x"))
+    // if paramter is not specified  value of Number(search_params.get("x")) is always zero
+    //console.log(Number(search_params.get("x"))+"hello")
+    if(maths == 0)
+      maths=Math.random()
+    res.write('Math.imul applied to '+maths+" is "+Math.imul(maths)+"\n");
+    res.write('Math.log applied to '+maths+" is "+Math.log(maths)+"\n");
+    res.end('Math.log10 applied to '+maths+" is "+Math.log10(maths));
+ });
+
+module.exports = router;
